25ec3e1906669e57765f1165c409f63e4f2d4845
nham <nick@wabbo.org>
2014-12-06 15:06:48 +0000

    Use docopt macro in nbody. Also allow initial position to be submitted. Syntax for commands has changed

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
+#![feature(phase)]
+
 extern crate serialize;
+
 extern crate docopt;
+#[phase(plugin)] extern crate docopt_macros;
 
 use std::num::Float;
 use docopt::Docopt;
 
-// Write the Docopt usage string.
-static USAGE: &'static str = "
-Usage: nbody [--dt STEP]
-       nbody [--dt STEP] <vx> <vy> <vz>
+docopt!(Args deriving Show, "
+Usage: nbody [options] [pos <x> <y> <z>] [vel <vx> <vy> <vz>]
 
 Options:
-    --dt STEP  Set the time step
-";
-
-#[deriving(Decodable, Show)]
-struct Args {
-    arg_vx: String,
-    arg_vy: String,
-    arg_vz: String,
-    flag_dt: String,
-}
+    --dt STEP     Set the time step
+")
 
 
 fn print_vals_pretty(x: f64, y: f64, z: f64, vx: f64, vy: f64, vz: f64) {
@@ -33,29 +27,36 @@ fn print_vals_bare(x: f64, y: f64, z: f64, vx: f64, vy: f64, vz: f64) {
 }
 
 fn main() {
-    let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| d.decode())
-                            .unwrap_or_else(|e| e.exit());
+    let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+    println!("{}", args);
+
     // if all the vx, vy, vz arguments are there, set initial velocity to it
     // otherwise use default
-    let vx: Option<f64> = from_str(args.arg_vx.as_slice());
-    let vy: Option<f64> = from_str(args.arg_vy.as_slice());
-    let vz: Option<f64> = from_str(args.arg_vz.as_slice());
-    let dt: Option<f64> = from_str(args.flag_dt.as_slice());
-
-    let (mut vx, mut vy, mut vz) = if vx.is_none() || vy.is_none() || vz.is_none() {
-        (0., 0.6, 0.) // default
+    let (mut x, mut y, mut z) = if args.cmd_pos == true {
+        let x: Option<f64> = from_str(args.arg_x.as_slice());
+        let y: Option<f64> = from_str(args.arg_y.as_slice());
+        let z: Option<f64> = from_str(args.arg_z.as_slice());
+        (x.unwrap_or(0.), y.unwrap_or(0.), z.unwrap_or(0.))
     } else {
-        (vx.unwrap(), vy.unwrap(), vz.unwrap())
+        (1., 0., 0.) // default
     };
 
+    let (mut vx, mut vy, mut vz) = if args.cmd_vel == true {
+        let vx: Option<f64> = from_str(args.arg_vx.as_slice());
+        let vy: Option<f64> = from_str(args.arg_vy.as_slice());
+        let vz: Option<f64> = from_str(args.arg_vz.as_slice());
+        (vx.unwrap_or(0.), vy.unwrap_or(0.), vz.unwrap_or(0.))
+    } else {
+        (0., 0.6, 0.) // default
+    };
+
+    let dt: Option<f64> = from_str(args.flag_dt.as_slice());
     let dt: f64 = if dt.is_none() {
-        0.01
+        0.01 //default
     } else {
         dt.unwrap()
     };
 
-    let (mut x, mut y, mut z): (f64, f64, f64) = (1., 0., 0.);
 
     println!("dt = {}", dt);
     print_vals_bare(x,y,z,vx,vy,vz);
